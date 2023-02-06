03c7fb91656bd222b7713e4fd48d463a83c809fe
Gonzalo <gonzalomaringomez@gmail.com>
2023-02-06 18:22:35 +0100

    Script acabado

--- script.sh
+++ script.sh
@@ -1,4 +1,9 @@
 #! /bin/sh
+sed -i 's/$this->user="root";/$this->user="'${USUARIO_BOOKMEDIK}'";/g' /var/www/html/core/controller/Database.php
+sed -i 's/$this->pass="";/$this->pass="'${CONTRA_BOOKMEDIK}'";/g' /var/www/html/core/controller/Database.php
+sed -i 's/$this->host="localhost";/$this->host="'${DATABASE_HOST}'";/g' /var/www/html/core/controller/Database.php
+sed -i 's/$this->ddbb="";/$this->ddbb="'${NOMBRE_DB}'";/g' /var/www/html/core/controller/Database.php
+sleep 5
 
 while ! mysql -u $USUARIO_BOOKMEDIK --password=$CONTRA_BOOKMEDIK -h $DATABASE_HOST -e ";" ;
 do sleep 1
